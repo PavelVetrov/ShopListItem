a8046a64fd2cdc0e75a9d5428736773a43c942cd
Pavel Vetrov <pauil@mail.ru>
2022-02-24 17:04:02 +0300

    realization data base

---
+++ app/src/main/java/com/example/shoppinglist/data/ShopListDao.kt
@@ -0,0 +1,22 @@
+package com.example.shoppinglist.data
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface ShopListDao {
+
+    @Query("SELECT*FROM shop_items")
+    fun getShopList(): LiveData<List<ShopItemDbModal>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun addShopItem(shopItemDbModal: ShopItemDbModal)
+
+    @Query("DELETE FROM shop_items WHERE id=:shopItemId")
+    fun deleteShopItem(shopItemId: Int)
+
+    @Query("SELECT*FROM shop_items WHERE id=:shopItemId LIMIT 1")
+    fun getShopItem(shopItemId: Int) :ShopItemDbModal
+
+
+}
--- app/src/main/java/com/example/shoppinglist/presentation/MainActivityViewModal.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/MainActivityViewModal.kt
@@ -3,10 +3,7 @@ package com.example.shoppinglist.presentation
 
 import androidx.lifecycle.ViewModel
 import com.example.shoppinglist.data.ShopListRepositoryImpl
-import com.example.shoppinglist.domain.DeleteShopItemUseCase
-import com.example.shoppinglist.domain.EditShopItemUseCase
-import com.example.shoppinglist.domain.GetShopListUseCase
-import com.example.shoppinglist.domain.ShopItem
+import com.example.shoppinglist.domain.*
 
 class MainActivityViewModal : ViewModel() {
 
@@ -15,6 +12,7 @@ class MainActivityViewModal : ViewModel() {
     private val getShopListUseCase = GetShopListUseCase(repository)
     private val deleteShopItemUseCase = DeleteShopItemUseCase(repository)
     private val editShopItemUseCase = EditShopItemUseCase(repository)
+
 
 
     val shopList = getShopListUseCase.getShopList()
